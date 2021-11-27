5e162b323efc1279cdfc7ad1e8932e8deab11a7c
Mahdi Nasser <mhnasser23@gmail.com>
2021-11-27 18:47:05 +0200

    greatly improved

--- script.js
+++ script.js
@@ -1,38 +1,104 @@
-let divs = [];
-const container = document.querySelector('.container');
+const grid = document.querySelector('.container');
+var dimension = 16;
+let currentMode = "rainbow";
+initial(dimension);
 
-//generate random number between min and max
-const randomBetween = (min, max) => min + Math.floor(Math.random() * (max - min + 1));
-
-function create(dimension){
-    for (i = 0; i < dimension**2; i++)
-    {
-        container.style.gridTemplateColumns= `repeat(${dimension}, 1fr)`;
-        
-        divs[i] = document.createElement('div');
-
-        //generate random rgb color
-        const r = randomBetween(0, 255);
-        const g = randomBetween(0, 255);
-        const b = randomBetween(0, 255);
-        
-        divs[i].addEventListener('mouseover', (e) => e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`);
-        container.appendChild(divs[i]);
+//intialize with 16x16 grid
+function initial(dimension){
+    grid.style.gridTemplateColumns= `repeat(${dimension}, 1fr)`;
+    grid.style.gridTemplateRows= `repeat(${dimension}, 1fr)`;
+    for (let i = 0; i < dimension**2; i++)
+    {    
+        divs = document.createElement('div');
+        grid.appendChild(divs);
     }
 }
-create(16);
 
-
-function clear(){
-    let dimension = 0;
-    let boxes = document.querySelectorAll('.container div');
-    boxes.forEach( box => box.remove());
-    do {
-    dimension = parseInt(prompt("Please enter squares' number per side (max 100) ", "16"));
-    } while (dimension < 1 || dimension > 100)
-    create(dimension);
+//generate random number between min and max
+function randomBetween(min, max) {
+    return min + Math.floor(Math.random() * (max - min + 1));
 }
 
-const button = document.querySelector('button');
-button.addEventListener('click', clear);
+//set all boxes to white
+function clear(){
+    let boxes = document.querySelectorAll('.container div');
+    boxes.forEach( box => box.style.backgroundColor = "white");
+ }
 
+//used later for dispatching events manually
+const clickEvent = new Event('click');
+const inputEvent = new Event('input');
+
+const buttonClear = document.querySelector('.clear');
+buttonClear.addEventListener('click', clear);
+
+const buttonColor = document.querySelector('.color');
+buttonColor.addEventListener('input', color);
+
+const rainbow = document.querySelector('.rainbow');
+rainbow.addEventListener('click', createRainbow);
+
+const eraser = document.querySelector('.eraser');
+eraser.addEventListener('click', erase);
+
+const sizeSlider = document.querySelector('.sizeSlider');
+sizeSlider.addEventListener('input', size);
+
+
+function color(color){
+    currentMode = "color";
+
+    let boxes = document.querySelectorAll('.container div');
+    boxes.forEach( box => box.addEventListener('mouseover', e => e.target.style.backgroundColor = `${color.target.value}`));
+}
+
+function createRainbow(){
+    currentMode = "rainbow";
+
+        let boxes = document.querySelectorAll('.container div');
+        boxes.forEach( box => {
+
+            //generate random rgb color each event
+            const r = randomBetween(0, 255);
+            const g = randomBetween(0, 255);
+            const b = randomBetween(0, 255);
+            box.addEventListener('mouseover', e => e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`)
+        });
+}
+
+function erase(){
+    currentMode = "erase";
+    let boxes = document.querySelectorAll('.container div');
+    boxes.forEach( box => box.addEventListener('mouseover', e => e.target.style.backgroundColor = 'white'));
+}
+
+function size(e){
+    dimension = e.target.value;
+
+    //remove all boxes
+    grid.innerHTML='';  
+    grid.style.gridTemplateColumns= `repeat(${dimension}, 1fr)`;
+    grid.style.gridTemplateRows= `repeat(${dimension}, 1fr)`;
+
+    //add desired boxes
+    for (let i = 0; i < dimension * dimension; i++)
+    {
+        const divs = document.createElement('div');
+        grid.appendChild(divs);
+    }
+
+    const sizeValue = document.getElementById('sizeValue');
+    sizeValue.innerHTML = `${dimension} x ${dimension}`;
+
+    //resume previous coloring option
+    if (currentMode == "color")
+        buttonColor.dispatchEvent(inputEvent);
+    else if (currentMode == "rainbow")
+        rainbow.dispatchEvent(clickEvent);
+    else
+        eraser.dispatchEvent(clickEvent);
+
+ }   
+
+
+  
