95a7aba46c3591414e6e10bc71db55e6fddfa134
Mahdi Nasser <mhnasser23@gmail.com>
2021-11-29 09:19:35 +0200

    fixed color button

--- script.js
+++ script.js
@@ -1,105 +1,110 @@
-const grid = document.querySelector('.container');
+const grid = document.querySelector(".container");
 var dimension = 16;
 let currentMode = "rainbow";
-//used later for dispatching events manually
-const clickEvent = new Event('click');
-const inputEvent = new Event('input');
+let singleColorButton = "black";
 
-const buttonClear = document.querySelector('.clear');
-buttonClear.addEventListener('click', clear);
+//used for dispatching events manually
+const clickEvent = new Event("click");
 
-const buttonColor = document.querySelector('.color');
-buttonColor.addEventListener('input', color);
+const buttonClear = document.querySelector(".clear");
+buttonClear.addEventListener("click", clear);
 
-const rainbow = document.querySelector('.rainbow');
-rainbow.addEventListener('click', createRainbow);
+const buttonColor = document.querySelector(".color");
+buttonColor.addEventListener("input", color);
+buttonColor.addEventListener("click", color);
 
-const eraser = document.querySelector('.eraser');
-eraser.addEventListener('click', erase);
+const rainbow = document.querySelector(".rainbow");
+rainbow.addEventListener("click", createRainbow);
 
-const sizeSlider = document.querySelector('.sizeSlider');
-sizeSlider.addEventListener('input', size);
+const eraser = document.querySelector(".eraser");
+eraser.addEventListener("click", erase);
 
+const sizeSlider = document.querySelector(".sizeSlider");
+sizeSlider.addEventListener("input", size);
 
 initial(dimension);
 //intialize with 16x16 grid and rainbow coloring
-function initial(dimension){
-    grid.style.gridTemplateColumns= `repeat(${dimension}, 1fr)`;
-    grid.style.gridTemplateRows= `repeat(${dimension}, 1fr)`;
-    for (let i = 0; i < dimension**2; i++)
-    {    
-        divs = document.createElement('div');
-        grid.appendChild(divs);
-    }
+function initial(dimension) {
+  grid.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${dimension}, 1fr)`;
+  for (let i = 0; i < dimension ** 2; i++) {
+    divs = document.createElement("div");
+    grid.appendChild(divs);
+  }
 
-    createRainbow();
+  createRainbow();
 }
 
 //generate random number between min and max
 function randomBetween(min, max) {
-    return min + Math.floor(Math.random() * (max - min + 1));
+  return min + Math.floor(Math.random() * (max - min + 1));
 }
 
 //set all boxes to white
-function clear(){
-    let boxes = document.querySelectorAll('.container div');
-    boxes.forEach( box => box.style.backgroundColor = "white");
- }
-
-function color(color){
-    currentMode = "color";
-
-    let boxes = document.querySelectorAll('.container div');
-    boxes.forEach( box => box.addEventListener('mouseover', e => e.target.style.backgroundColor = `${color.target.value}`));
+function clear() {
+  let boxes = document.querySelectorAll(".container div");
+  boxes.forEach((box) => (box.style.backgroundColor = "white"));
 }
 
-function createRainbow(){
-    currentMode = "rainbow";
+function color(color) {
+  currentMode = "color";
+  if (color.type == "input") singleColorButton = color.target.value;
 
-        let boxes = document.querySelectorAll('.container div');
-        boxes.forEach( box => {
-
-            //generate random rgb color each event
-            const r = randomBetween(0, 255);
-            const g = randomBetween(0, 255);
-            const b = randomBetween(0, 255);
-            box.addEventListener('mouseover', e => e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`)
-        });
+  let boxes = document.querySelectorAll(".container div");
+  boxes.forEach((box) =>
+    box.addEventListener(
+      "mouseover",
+      (e) => (e.target.style.backgroundColor = `${singleColorButton}`)
+    )
+  );
 }
 
-function erase(){
-    currentMode = "erase";
-    let boxes = document.querySelectorAll('.container div');
-    boxes.forEach( box => box.addEventListener('mouseover', e => e.target.style.backgroundColor = 'white'));
+function createRainbow() {
+  currentMode = "rainbow";
+
+  let boxes = document.querySelectorAll(".container div");
+  boxes.forEach((box) => {
+    //generate random rgb color each event
+    const r = randomBetween(0, 255);
+    const g = randomBetween(0, 255);
+    const b = randomBetween(0, 255);
+    box.addEventListener(
+      "mouseover",
+      (e) => (e.target.style.backgroundColor = `rgb(${r}, ${g}, ${b})`)
+    );
+  });
 }
 
-function size(e){
-    dimension = e.target.value;
+function erase() {
+  currentMode = "erase";
+  let boxes = document.querySelectorAll(".container div");
+  boxes.forEach((box) =>
+    box.addEventListener(
+      "mouseover",
+      (e) => (e.target.style.backgroundColor = "white")
+    )
+  );
+}
 
-    //remove all boxes for better performance
-    grid.innerHTML='';  
-    grid.style.gridTemplateColumns= `repeat(${dimension}, 1fr)`;
-    grid.style.gridTemplateRows= `repeat(${dimension}, 1fr)`;
+function size(e) {
+  dimension = e.target.value;
 
-    //add desired boxes
-    for (let i = 0; i < dimension * dimension; i++)
-    {
-        const divs = document.createElement('div');
-        grid.appendChild(divs);
-    }
+  //remove all boxes for better performance
+  grid.innerHTML = "";
+  grid.style.gridTemplateColumns = `repeat(${dimension}, 1fr)`;
+  grid.style.gridTemplateRows = `repeat(${dimension}, 1fr)`;
 
-    const sizeValue = document.getElementById('sizeValue');
-    sizeValue.innerHTML = `${dimension} x ${dimension}`;
+  //add desired boxes
+  for (let i = 0; i < dimension * dimension; i++) {
+    const divs = document.createElement("div");
+    grid.appendChild(divs);
+  }
+  //adjust scroller text
+  const sizeValue = document.getElementById("sizeValue");
+  sizeValue.innerHTML = `${dimension} x ${dimension}`;
 
-    //resume previous coloring option
-    if (currentMode == "color")
-        buttonColor.dispatchEvent(inputEvent);
-    else if (currentMode == "rainbow")
-        rainbow.dispatchEvent(clickEvent);
-    else
-        eraser.dispatchEvent(clickEvent);
-
- }   
-
-
-  
+  //resume previous coloring option
+  if (currentMode == "color") buttonColor.dispatchEvent(clickEvent);
+  else if (currentMode == "rainbow") rainbow.dispatchEvent(clickEvent);
+  else eraser.dispatchEvent(clickEvent);
+}
